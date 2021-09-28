25ee2ed093d07c46a432917edae9cc5ddc3acf1a
天野彰吾 <shogoa301@gmail.com>
2021-09-28 10:47:18 +0900

    ドットインストールVue 6/17

---
+++ js/main.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+
+
+    //
+    // ①vueの特徴
+    // 双方向データバインディング
+    //　データを書き換えればUIにも反映される
+
+
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            newItem:'',
+            name: 'amano',
+            todos: [
+                'task1',
+                'task2',
+                'task3'
+            ]
+        },
+        methods:{
+            addItem: function() {
+                this.todos.push(this.newItem);
+                this.newItem = '';
+            }
+        }
+    });
+})();

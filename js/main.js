17c5c52492d71c274df6295c5516e35e75367803
天野彰吾 <shogoa301@gmail.com>
2021-09-30 01:43:15 +0900

    13/17

--- js/main.js
+++ js/main.js
@@ -8,29 +8,30 @@
             newItem:'',
             name: 'amano',
             todos: [
-                {
-                tit: 'task1',
-                done: false
-                },
-                {
-                tit: 'task2',
-                done: false
-                },
-                {
-                tit: 'task3',
-                done: true
-                },
+                // {
+                // tit: 'task1',
+                // done: false
+                // },
+                // {
+                // tit: 'task2',
+                // done: false
+                // },
+                // {
+                // tit: 'task3',
+                // done: true
+                // },
             ]
         },
         watch:{
             todos: {
                 handler: function(){
                     localStorage.setItem('todos', JSON.stringify(this.todos));
-                    // alert('保存した！');
                 },
                 deep: true
-                // deepwatcherを使う。→要素の中身も変更したら更新かかるようにする
             }
+        },
+        mounted: function(){
+            this.todos = JSON.parse(localStorage.getItem('todos')) || [];
         },
         methods:{
             addItem: function() {
